e379d87334c544bfc7d15caeec04c0b4d0679546
root <diego.iron66@hotmail.com>
2017-07-19 16:19:47 -0400

    calendario

---
+++ js/site.js
@@ -0,0 +1,57 @@
+var clndr = {};
+
+$( function() {
+
+  // PARDON ME while I do a little magic to keep these events relevant for the rest of time...
+  var currentMonth = moment().format('YYYY-MM');
+  var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+
+  var events = [
+    { date: currentMonth + '-' + '10', title: 'Cover', location: 'Kroplsbierhaus' },
+    { date: currentMonth + '-' + '15', title: 'Winter Bierfest', location: 'Kroplsbierhaus' },
+     { date: currentMonth + '-' + '19',  title: 'Reunion', location: 'Kroplsbierhaus' },
+    { date: currentMonth + '-' + '22', title: 'Friends & Beer Fiesta Invernal de la Cerveza Artesanal', location: 'Kroplsbierhaus' },
+
+    { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
+
+      ];
+
+  clndr = $('#full-clndr').clndr({
+    template: $('#full-clndr-template').html(),
+    events: events,
+    forceSixRows: true
+  });
+
+  $('#mini-clndr').clndr({
+    template: $('#mini-clndr-template').html(),
+    events: events,
+    clickEvents: {
+      click: function(target) {
+        if(target.events.length) {
+          var daysContainer = $('#mini-clndr').find('.days-container');
+          daysContainer.toggleClass('show-events', true);
+          $('#mini-clndr').find('.x-button').click( function() {
+            daysContainer.toggleClass('show-events', false);
+          });
+        }
+      }
+    },
+    adjacentDaysChangeMonth: true,
+    forceSixRows: true
+  });
+
+  $('#clndr-3').clndr({
+    template: $('#clndr-3-template').html(),
+    events: events,
+    showAdjacentMonths: false
+  });
+
+  // $('#clndr-4').clndr({
+  //   template: $('#clndr-4-template').html(),
+  //   events: events,
+  //   lengthOfTime: {
+  //     days: 7,
+  //     interval: 7
+  //   }
+  // });
+});
